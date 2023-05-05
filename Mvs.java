39cb4511a049b6dfce8904e080fb9935ad789b7f
sandeepk68 <130872773+sandeepk68@users.noreply.github.com>
2023-05-05 19:59:54 -0400

    Create Mvs.java

---
+++ Mvs.java
@@ -0,0 +1,20 @@
+// Mouse Jiggler in Java
+// Ctrl+c to exit the program
+
+import java.awt.Robot;
+import java.util.Random;
+
+public class Mvs {
+    public static final int FIVE_SECONDS = 5000;
+    public static final int MAX_Y = 400;
+    public static final int MAX_X = 400;
+
+    public static void main(String... args) throws Exception {
+        Robot robot = new Robot();
+        Random random = new Random();
+        while (true) {
+            robot.mouseMove(random.nextInt(MAX_X), random.nextInt(MAX_Y));
+            Thread.sleep(FIVE_SECONDS);
+        }
+    }
+}
