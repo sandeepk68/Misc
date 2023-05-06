e58a79208afa5c2eca063f7aa66a05fea9e51c19
sandeepk68 <130872773+sandeepk68@users.noreply.github.com>
2023-05-05 20:14:29 -0400

    Added time check functionality

--- Mvs.java
+++ Mvs.java
@@ -3,18 +3,31 @@
 
 import java.awt.Robot;
 import java.util.Random;
+import java.time.LocalDateTime;
 
 public class Mvs {
-    public static final int FIVE_SECONDS = 5000;
+    public static final int FIVE_MINUTES = 300_000;
+    public static final int SET_TIME_HOURS = 17;
     public static final int MAX_Y = 400;
     public static final int MAX_X = 400;
 
     public static void main(String... args) throws Exception {
+        
+        int hour;
+        int minute;
+        
         Robot robot = new Robot();
         Random random = new Random();
-        while (true) {
+        
+        do {
             robot.mouseMove(random.nextInt(MAX_X), random.nextInt(MAX_Y));
-            Thread.sleep(FIVE_SECONDS);
-        }
+            Thread.sleep(FIVE_MINUTES);
+            
+            LocalDateTime now = LocalDateTime.now();
+            hour = now.getHour();
+            minute = now.getMinute();
+            
+            // System.out.println(hour:minute);
+        } while (hour != SET_TIME_HOURS)
     }
 }
